d4599c5b6b7bfa5ec1096dcb3d6b4013fc6bcdd2
Rodrigo Natan <rnattan82@gmail.com>
2024-07-21 17:12:26 -0300

    Pagina inicial finalizada

--- slots/src/pages/home/index.jsx
+++ slots/src/pages/home/index.jsx
@@ -1,10 +1,19 @@
-
 import './styles.css';
+import avatar from '../../assets/avatar.png';
+import logo from '../../assets/logo.svg';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='container_home'>
+      <img src={logo} alt="img_logo" className='logo' />
+      <div className='card'>
+        <img src={avatar} alt="avatar" className='avatar' />
+        <h1>Olá Jogadores</h1>
+        <h2>Sejam muito bem vindos</h2>
+        <h3>Ao nosso site</h3>
+
+        <button>Acessar</button>
+      </div>
     </div>
   );
 }
